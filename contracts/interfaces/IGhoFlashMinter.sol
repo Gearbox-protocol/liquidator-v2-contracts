82b69cbb78a1587713517feee24ea27f809e82c0
doomsower <12031673+doomsower@users.noreply.github.com>
2025-04-01 16:22:00 -0300

    feat: new contracts

--- contracts/interfaces/IGhoFlashMinter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-// Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Foundation, 2024
-pragma solidity ^0.8.10;
-
-interface IGhoFlashMinter {
-    function flashLoan(address receiver, address token, uint256 amount, bytes calldata data) external returns (bool);
-}
